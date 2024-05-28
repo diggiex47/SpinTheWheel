83c57367072413e859c15ba5c80fc3865a6b130a
dazzy <77917981+diggiex47@users.noreply.github.com>
2024-05-28 08:52:54 +0530

    added further functionality

--- project.js
+++ project.js
@@ -54,7 +54,7 @@ const getNumberOfLines = () =>{
             return numberOfLines;
         }
     }
-}
+};
 
 const getBet = (balance) => {
     while (true) {
@@ -68,15 +68,54 @@ const getBet = (balance) => {
             return numberBet;
         }
     }
-}
+};
 
 
 const spin = () => {
     const symbols = [];
+    for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)){
+        for (let i = 0; i < count; i++) {
+            symbols.push (symbol);
+        }
+    }
+    const reels = [];
+    for (let i =0; i< COLS; i++){
+        reels.push([])
+        const reelSymbols = [...symbols];
+        for (let j = 0; j < ROWS; j++) {
+            const randomIndex = Math.floor(Math.random() * reelSymbols.length);
+            const selectedSymbol = reelSymbols[randomIndex];
+            reels[i].push(selectedSymbol);
+            reelSymbols.splice(randomIndex, 1);
+        }
+    }
+    return reels;
+};
 
-}
+
+const transpose = (reels) => {
+    const rows = [];
+    
+    for (let i =0; i<ROWS; i++) {
+        rows.push([]);
+        for (let j =0; j<COLS; j++) {
+            rows[i].push(reels[j][i]);
+        }
+    }
+    return rows;
+};
+
+const printRows = (rows) => {
+    for (const row of rows){
+        let rowString = "A | B | C";
+    }
+};
 
 
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
 const bet = getBet(balance, numberOfLines);
+const reels = spin();
+const rows = transpose(reels);
+console.log(reels);
+console.log(rows);
